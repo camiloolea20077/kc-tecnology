74fce5f409040c383fa5a0da40816f9ff09079a0
navarro243 <126024824+navarro243@users.noreply.github.com>
2025-01-08 13:02:31 -0500

    feat: Arreglos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PrincipalComponent } from './pages/principal/principal.component';
 import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { ServicesComponent } from './pages/servicios/services.component'
 
 
 
@@ -9,6 +10,7 @@ const routes: Routes = [
   {path:'about-us', component:AboutUsComponent},
   {path:'',redirectTo:'/principal', pathMatch:'full'},
   {path:'principal',component:PrincipalComponent},
+  {path:'servicios', component:ServicesComponent}
   
 ]
 
