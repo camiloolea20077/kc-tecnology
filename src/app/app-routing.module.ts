f116d8fedeee410f5a0cf473312d9f39dc0817cd
camiloolea20077 <112830645+camiloolea20077@users.noreply.github.com>
2025-01-08 21:22:41 -0500

    ajuste de nav e info

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PrincipalComponent } from './pages/principal/principal.component';
-import { AboutUsComponent } from './pages/about-us/about-us.component';
-import { ServicesComponent } from './pages/servicios/services.component'
-
-
 
 const routes: Routes = [
-  {path:'about-us', component:AboutUsComponent},
-  {path:'',redirectTo:'/principal', pathMatch:'full'},
-  {path:'principal',component:PrincipalComponent},
-  {path:'servicios', component:ServicesComponent}
-  
-]
+  { path: '', redirectTo: '/principal', pathMatch: 'full' },
+  {
+    path: 'about-us',
+    loadComponent: () =>
+      import('./pages/about-us/about-us.component').then(
+        (m) => m.AboutUsComponent
+      ),
+  },
+  { path: 'principal', component: PrincipalComponent },
+  {
+    path: 'servicios',
+    loadComponent: () =>
+      import('./pages/servicios/services.component').then(
+        (m) => m.ServicesComponent
+      ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { PrincipalComponent } from "../../pages/principal/principal.component";
 
 @Component({
     selector: 'app-navbar',
     standalone: true,
     templateUrl: './navbar.component.html',
     styleUrls: ['./navbar.component.scss'],
-    imports: [RouterModule]
+    imports: [RouterModule, PrincipalComponent]
 })
 export class NavbarComponent {
     constructor() {}
